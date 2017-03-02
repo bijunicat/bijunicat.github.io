321c0bbfff0c187e16e4c4e262c6226000cd0a22
vladwho <vladpresedinte@gmail.com>
2017-03-02 22:50:52 +0200

    Deploy from handmadecat

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem "jekyll-assets"
+gem "uglifier"
+gem "sass"
+gem "nokogiri", ">= 1.6.7.rc3"
---
+++ _assets/js/main.js
@@ -0,0 +1,5 @@
+//=require jquery.easing.min.js
+//=require jquery.fittext.js
+//=require wow.min.js
+//=require creative.js
+//=require lightbox.min.js

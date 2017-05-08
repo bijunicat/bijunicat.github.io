46564c3b91b80d9e784b97bfb27b4f78a3d1a42b
vladwho <vladpresedinte@gmail.com>
2017-05-08 19:19:33 +0300

    Remove jekyll-assets plugin

--- Gemfile
+++ Gemfile
@@ -2,4 +2,3 @@ source "https://rubygems.org"
 ruby RUBY_VERSION
 
 gem 'github-pages'
-gem 'jekyll-assets'
--- _assets/js/main.js
+++
@@ -1,5 +0,0 @@
-//=require jquery.easing.min.js
-//=require jquery.fittext.js
-//=require wow.min.js
-//=require creative.js
-//=require lightbox.min.js
